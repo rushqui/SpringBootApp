190a924fdcb0e087bd0717b5bb988f6c5489863e
Carlos Elias Rojas Aldana <carlos_rojasa@hacienda.gob.mx>
2019-04-16 19:20:45 -0500

    first commit

---
+++ rushquimechanix/src/main/java/com/rushquimechanix/licenses/RushquimechanixApplication.java
@@ -0,0 +1,15 @@
+package com.rushquimechanix.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RushquimechanixApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushquimechanixApplication.class, args);
+	}
+
+}
+
